5f1662e9e3828d228780ee22610c7c86f753ea94
chandanosscube <chandan.kumar@osscube.com>
2013-08-01 23:33:14 -0700

    footer page

---
+++ View/footer.php
@@ -0,0 +1,57 @@
+<head>
+<style>
+#footer {
+    
+	color:white;
+	 box-shadow: -13px -12px 11px #DEB887;
+   background-image:url('footer.png');	
+	background-size: 100%;
+	background-repeat: no-repeat;
+}
+
+#navcontainer li:before{
+    content:" * ";
+}
+#navcontainer ul li {
+	display: inline;
+	margin: 3px;
+	list-style-type: circle;
+	
+
+
+}
+
+#footer_subtitle
+{
+font-family:Serif;font-style:italic;	
+	text-align: center;
+}
+</style>
+
+</head>
+<body>
+<div id="footer">
+<h2 id="footer_subtitle">Team Lead:
+Chetan Sharma
+</h2>
+<b>Design &amp; Developed By:</b><div id="navcontainer">
+<ul>
+<li>Amber Sharma</li>
+<li>Mohit Gupta</li>
+<li>Gaurav Kumar Sharma</li>
+<li>Kawaljeet Singh</li>
+<li>Anirudh Pandita</li>
+<li>Avni Jain</li>
+<li>
+Keshi Chander Yadava</li>
+<li>Chandan Kumar</li>
+<li>Mohit Kumar Singh</li>
+<li>Prateek Saini</li>
+<li>Ramandeep Singh Mann</li>
+<li>Debanshu Kar</li>
+<li>Sanchit Puri</li>
+</ul>
+
+</div>
+</div>
+</body>
