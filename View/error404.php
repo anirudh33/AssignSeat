e5288bad8c65bd412644282ceaff5bbbdc895e92
chandanosscube <chandan.kumar@osscube.com>
2013-08-08 01:15:43 -0700

    error page

--- View/error404.php
+++ View/error404.php
@@ -1,33 +1,14 @@
-<style>
-.error404{
-	
-	margin-left: 150px;
-	margin-top: 100px;
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 
-    height:100px;
-	
-}
-.margin404{
-	margin-left: 20px;
-}
-.leftmarginerror
-{
-	
-	padding-left: 30px;
-}
-
-
-</style>
-<body bgcolor="#D8C2AB">
 <table class="error404">
 <tr>
-<td><img src="images/alert.png" height="60px" width="60px"></td><td><div class="margin404"><h2>Error 404</h2></div></td>
+<td><img src="../images/alert.png" height="100px" width="100px"></td><td><div class="margin404">Error 404</div></td>
 
 </tr>
 
 <tr>
-<td></td><td class="leftmarginerror">The requested URL could not be retrieved</td>
+<td></td><td class="leftmarginerror">The requested URL could not be retrieved.</td>
 </tr>
 
 </table>
-</body>
+
