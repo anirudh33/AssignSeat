4d821ede0afdd99921424ac569b0861cd6431cd8
chandanosscube <chandan.kumar@osscube.com>
2013-08-08 02:40:58 -0700

    another error page

--- View/error404.php
+++ View/error404.php
@@ -1,14 +1,38 @@
+<script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>
 <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+<SCRIPT>
+$( document ).ready(function() {
+$('#testerror').load(function() {
+	
+    $(this).data('height', this.height);
+}).bind('mouseenter mouseleave', function(e) {
+    var enter = e.type === 'mouseenter',
+        height = $(this).data('height');
+    
+    $(this).stop().animate({
+        'margin-top': (enter ? 10 : 0),
+        opacity: (enter ? 5 : 0.9),
+        height: height * (enter ? 1.55 : 1)
+        	
+    });
+});
+});
+</SCRIPT>
+<style>
 
-<table class="error404">
-<tr>
-<td><img src="../images/alert.png" height="100px" width="100px"></td><td><div class="margin404">Error 404</div></td>
+.errorPagediv
+{
+	margin: 80px;
+padding: 10px;
+	
+}
+#testerror{
+	border-radius:10px;
+}
+</style>
 
-</tr>
 
-<tr>
-<td></td><td class="leftmarginerror">The requested URL could not be retrieved.</td>
-</tr>
-
-</table>
-
+<div class="errorPagediv">
+<img src="../images/404.jpg"  width="98%" height="45%" id="testerror">
+</div>
+</body>
---
+++ View/error404page.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+
+<table class="error404">
+<tr>
+<td><img src="../images/alert.png" height="100px" width="100px"></td><td><div class="margin404">Error 404</div></td>
+
+</tr>
+
+<tr>
+<td></td><td class="leftmarginerror">The requested URL could not be retrieved.</td>
+</tr>
+
+</table>
+
