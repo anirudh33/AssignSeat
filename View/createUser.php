3cd5b8f37c2ac7decc428aca4ded07ded7de6b74
oesOsscube <debanshu.kar@osscube.com>
2013-08-13 00:00:29 -0700

    Admin

---
+++ View/createUser.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+</head>
+
+<body>
+  <table>
+   <tr>
+    <td>User Login Name : </td> <td><input type="text" name="user" required="required"></td>
+   </tr>
+   <tr>
+    <td>Password : </td> <td><input type="password" name="password" required="required"></td>
+   </tr>
+   <tr>
+    <td></td> <td><input type="submit" value="Create User"></td>
+   </tr>
+  </table>
+</body>
+</html>
