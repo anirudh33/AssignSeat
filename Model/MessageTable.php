1df7072c45299311ad8343fd46130bb11c83af63
chandanosscube <chandan.kumar@osscube.com>
2013-08-15 16:08:59 -0700

    class for table creation

--- Model/MessageTable.php
+++ Model/MessageTable.php
@@ -1,12 +1,21 @@
 <?php
 class MessageTable{
  protected $tableForm;
-	public function createTable($tableMsg,$msg="",$noOfCol)
+	public function createTable($tableMsg,$msg="",$noOfCol=1)
 	{
 $this->tableForm="<table width='400'><caption>".$msg."</caption>";
 		foreach($tableMsg as $key=>$value)
 		{
-			$this->tableForm.="<tr><td>".$value."</td></tr>";			
+			$this->tableForm.="<tr>";
+			if($noOfCol==1)
+			{
+			$this->tableForm.="<td>".$value."</td>";	
+			}
+			else{
+				//write your code according to key value pair
+			}
+			$this->tableForm.="</tr>";
+					
 		}
 		$this->tableForm.="</table>";
 		return $this->tableForm;
